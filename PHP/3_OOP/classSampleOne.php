b206efa909a3cdc8d002176fa2dc2b41e54685ec
P340-1 <liangabarsky@yandex.ru>
2021-11-15 02:24:49 +0300

    Отредактирован пример PHP ООП

--- PHP/3_OOP/classSampleOne.php
+++ PHP/3_OOP/classSampleOne.php
@@ -6,10 +6,14 @@
     private   $val_1;
     protected $val_2;
 
+    public static $val_static = 'staticval';
+
+    const val_const = 'const';
+
     public function __construct()
     {
         $this->$val_0 = 'val_0';
-        echo('construct '.$this->$val_0);
+        echo('construct '.$this->$val_0.'<br>');
     }
 
     public function msg()
@@ -18,21 +22,13 @@
     }
  }
 
-
- class classSampleTwo
- {
-     public function msg()
-     {
-         echo('SampleTwo...');
-     }
- }
-
-
+ 
  interface interfaceSample 
  {
      public function printVal();
  }
 
+ 
  trait traitSample
  {
      function traitPrintVal()
@@ -42,3 +38,53 @@
  }
 
  
+ class classSampleTwo extends classSampleOne
+ {
+     public function msg()
+     {
+         echo('SampleTwo...');
+     }
+ }
+
+
+ abstract class classSampleThree
+ {
+    const val_c = 'C';
+    
+    public static $val_abs_static = 'vas';
+    public abstract function __construct();
+    public abstract function __destruct();
+
+    abstract public function fabs();
+    abstract public function traitPrintVal();
+    public function fnoabs() 
+    {
+        echo('<br> no abstract function in abstract classThree... ');
+    }
+ }
+
+
+ class classSampleFour extends classSampleThree implements interfaceSample
+ {
+
+    use traitSample;
+
+    function __construct()
+     {
+         echo('<br> construct Four Obj ');
+     }
+
+     function __destruct()
+     {
+        echo('<br> destruct Four Obj ');
+     }
+
+    function fabs() { echo(' <br> fabs'); }
+
+    function printVal() 
+    {
+
+    }
+ }
+
+
--- PHP/3_OOP/index.php
+++ PHP/3_OOP/index.php
@@ -3,15 +3,41 @@ spl_autoload_register();
 error_reporting(E_ALL);
 
 $obj = new classSampleOne();
-echo $obj->msg();
+echo $obj->msg(); 
 echo('<br>');
 
-echo( ($obj->$val_0).'<br>');
+echo( $obj->$val_0);
+echo('<br>');
 
 $obj = new classSampleTwo();
 $obj->$newVal = 'newVal';
 echo $obj->msg();
 echo('<br>');
 echo($obj->$newVal);
+echo('<br>');
 
+echo('const: ');
+$obj->const_val = ' my_const'; // ???
+$obj->const_val = ' my_const1';
 
+echo($obj->const_val);
+echo('<br>');
+
+echo('const: ');
+echo(classSampleOne::val_const);
+echo('<br>');
+
+echo('static val: ');
+echo(classSampleOne:: $val_static);
+echo('<br>');
+
+echo('static two: ');
+echo(classSampleTwo:: $val_static);
+echo('<br>');
+
+$obj = new classSampleFour();
+$obj->fabs();
+$obj->fnoabs();
+$obj->traitPrintVal();
+
+// Создать анонимный класс
